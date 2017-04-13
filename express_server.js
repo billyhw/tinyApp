4df732de14fc706536347ee3399f180196ab836a
Billy Chang <billyheungwing@yahoo.ca>
2017-04-13 15:58:58 +0000

    added a const users object

--- express_server.js
+++ express_server.js
@@ -12,6 +12,19 @@ var urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@examle.com",
+    password: "dishwasher-funk"
+  }
+}
+
 app.get('/', (req, res) => {
   res.end("Hello!");
 });
